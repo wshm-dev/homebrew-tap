084c61f794c6b33ddaac2481cf417c5f472d1b34
patrick szymkowiak <52030887+pszymkowiak@users.noreply.github.com>
2026-05-19 13:33:23 +0000

    wshm-pro 0.31.14-pro

--- Formula/wshm-pro.rb
+++ Formula/wshm-pro.rb
@@ -1,17 +1,17 @@
 class WshmPro < Formula
   desc "AI-powered repository agent — PR corrections, changelogs, reports"
   homepage "https://wshm.dev"
-  version "0.31.13-pro"
+  version "0.31.14-pro"
   license "LicenseRef-Proprietary"
 
   on_linux do
     on_intel do
-      url "https://github.com/wshm-dev/homebrew-tap/releases/download/v0.31.13-pro/wshm-pro-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "4198abbad3b3f1c4f9604eef3f4648f794e14158ef61bae5c15fee867af034c8"
+      url "https://github.com/wshm-dev/homebrew-tap/releases/download/v0.31.14-pro/wshm-pro-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "017782fef0a063983ba8c14592141428a0a338e6979e73bfe144496c3c211cb9"
     end
     on_arm do
-      url "https://github.com/wshm-dev/homebrew-tap/releases/download/v0.31.13-pro/wshm-pro-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "96013dd15724b68ead207786634d9fbefe5511915f893eebbc4e03cc36cd944a"
+      url "https://github.com/wshm-dev/homebrew-tap/releases/download/v0.31.14-pro/wshm-pro-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "4d0462c00f6be30fef66660b60fcd338d3fb79bd730b03aa1e66584de1175424"
     end
   end
 
